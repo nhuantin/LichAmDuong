a297f93cecc80edd37ceb0d239e3e4d599796dd9
nhuantin <nhuantincqta@gmail.com>
2025-05-15 20:25:08 +0700

    Delete calendarapp directory

--- calendarapp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import calendar_view  # Import trực tiếp hàm view
-
-urlpatterns = [
-    path("", calendar_view, name="calendar"),  # Định nghĩa URL pattern
-]
